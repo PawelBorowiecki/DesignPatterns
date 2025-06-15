635c281716856e2f80735b9dbb6da4a4af413fd2
Paweł Borowiecki <161360528+PawelBorowiecki@users.noreply.github.com>
2025-06-15 12:07:34 +0200

    Adapter uploaded

---
+++ Adapter/Adapter/src/main/java/org/example/Euro.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Euro {
+    private double quantity;
+
+    public Euro(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+}
